83a530a6c8acd30ffe35afab4e189b85d6d6b3b0
Carolina López <clopez@stackbuilders.com>
2022-11-09 18:40:34 -0500

    Compatible with Laravel 7, different names in routes

--- src/Filevuer.php
+++ src/Filevuer.php
@@ -9,51 +9,49 @@ class Filevuer
     public static function routes()
     {
         $namespace = '\\jwhulette\\filevuer\\controllers\\';
-        
+
         Route::group([
             'prefix' => 'filevuer',
             'middleware' => [
                 'web',
                 'sessionDriver'
             ],
+        ], function () use ($namespace) {
+            Route::get('/', $namespace . 'FilevuerController@index')->name('filevuer.index');
+
+            Route::get('/poll', $namespace . 'FilevuerController@poll')->name('filevuer.poll');
+
+            Route::get('/logout', $namespace . 'FilevuerController@logout')->name('filevuer.logout');
+
+            Route::post('/', $namespace . 'FilevuerController@connect')->name('filevuer.hash');
+
+            Route::post('/download', $namespace . 'DownloadController@generate')->name('filevuer.generate');
+
+            Route::get('/download/{hash}', $namespace . 'DownloadController@download')->name('filevuer.download');
+
+            Route::post('/upload', $namespace . 'UploadController@upload')->name('filevuer.upload');
+
+            Route::group([
+                'prefix' => 'file'
             ], function () use ($namespace) {
-                Route::get('/', $namespace . 'FilevuerController@index')->name('filevuer.index');
+                Route::get('/', $namespace . 'FileController@show')->name('filevuer.file.show');
 
-                Route::get('/poll', $namespace . 'FilevuerController@poll')->name('filevuer.poll');
+                Route::post('/', $namespace . 'FileController@create')->name('filevuer.file.create');
 
-                Route::get('/logout', $namespace . 'FilevuerController@logout')->name('filevuer.logout');
+                Route::put('/', $namespace . 'FileController@update')->name('filevuer.file.update');
 
-                Route::post('/', $namespace . 'FilevuerController@connect')->name('filevuer.hash');
-        
-                Route::post('/download', $namespace . 'DownloadController@generate')->name('filevuer.generate');
-
-                Route::get('/download/{hash}', $namespace . 'DownloadController@download')->name('filevuer.download');
-
-                Route::post('/upload', $namespace . 'UploadController@upload')->name('filevuer.upload');
-        
-                Route::group([
-                'prefix' => 'file',
-                'as' => 'filevuer.file'
-                ], function () use ($namespace) {
-                    Route::get('/', $namespace . 'FileController@show');
-                    
-                    Route::post('/', $namespace . 'FileController@create');
-
-                    Route::put('/', $namespace . 'FileController@update');
-
-                    Route::delete('/', $namespace . 'FileController@destroy');
-                });
-        
-                Route::group([
-                'prefix' => 'directory',
-                'as' => 'filevuer.directory'
-                ], function () use ($namespace) {
-                    Route::get('/', $namespace . 'DirectoryController@index');
-
-                    Route::post('/', $namespace . 'DirectoryController@create');
-
-                    Route::delete('/', $namespace . 'DirectoryController@destroy');
-                });
+                Route::delete('/', $namespace . 'FileController@destroy')->name('filevuer.file.destroy');
             });
+
+            Route::group([
+                'prefix' => 'directory'
+            ], function () use ($namespace) {
+                Route::get('/', $namespace . 'DirectoryController@index')->name('filevuer.directory');
+
+                Route::post('/', $namespace . 'DirectoryController@create')->name('filevuer.directory.create');
+
+                Route::delete('/', $namespace . 'DirectoryController@destroy')->name('filevuer.directory.destroy');
+            });
+        });
     }
 }
--- tests/Feature/FileControllerTest.php
+++ tests/Feature/FileControllerTest.php
@@ -13,7 +13,7 @@ class FileControllerTest extends TestCase
         parent::setUp();
         $filesystem = $this->getMockBuilder(FilesystemManager::class)
             ->disableOriginalConstructor()
-            ->setMethods(['cloud', 'read','put','delete'])
+            ->setMethods(['cloud', 'read', 'put', 'delete'])
             ->getMock();
         $filesystem->method('cloud')
             ->will($this->returnSelf());
@@ -28,7 +28,8 @@ class FileControllerTest extends TestCase
 
     public function testShow()
     {
-        $response = $this->withSession($this->getSessionValues())->get(route('filevuer.file'), [ 'path' => '']);
+        $response = $this->withSession($this->getSessionValues())
+            ->get(route('filevuer.file.show'), ['path' => '']);
 
         $response->assertStatus(200);
         $this->assertEquals(json_encode([
@@ -40,7 +41,7 @@ class FileControllerTest extends TestCase
     public function testShowFailed()
     {
         session()->forget(SessionInterface::FILEVUER_HOME_DIR);
-        $response = $this->put(route('filevuer.file'), [ 'path' => null]);
+        $response = $this->put(route('filevuer.file.show'), ['path' => null]);
 
         $response->assertStatus(500);
     }
@@ -48,7 +49,7 @@ class FileControllerTest extends TestCase
 
     public function testCreate()
     {
-        $response = $this->withSession($this->getSessionValues())->post(route('filevuer.file'), [ 'path' => '']);
+        $response = $this->withSession($this->getSessionValues())->post(route('filevuer.file.create'), ['path' => '']);
 
         $response->assertStatus(201);
         $this->assertEquals('{"success":true}', $response->getContent());
@@ -56,7 +57,7 @@ class FileControllerTest extends TestCase
 
     public function testUpdate()
     {
-        $response = $this->withSession($this->getSessionValues())->put(route('filevuer.file'), [ 'path' => '', 'contents' => 'new contents']);
+        $response = $this->withSession($this->getSessionValues())->put(route('filevuer.file.update'), ['path' => '', 'contents' => 'new contents']);
 
         $response->assertStatus(200);
         $this->assertEquals('{"success":true}', $response->getContent());
@@ -64,7 +65,7 @@ class FileControllerTest extends TestCase
 
     public function testDelete()
     {
-        $response = $this->withSession($this->getSessionValues())->delete(route('filevuer.file'), [ 'path' => ['test.txt']]);
+        $response = $this->withSession($this->getSessionValues())->delete(route('filevuer.file.destroy'), ['path' => ['test.txt']]);
 
         $response->assertStatus(200);
         $this->assertEquals('{"success":true}', $response->getContent());
