a923b69d8db55cb443240eda55ae51d88e8b6239
Alvaro <alvaro96rojo@gmail.com>
2023-03-01 10:43:56 +0100

    Custom sort

--- src/services/DirectoryService.php
+++ src/services/DirectoryService.php
@@ -38,9 +38,8 @@ class DirectoryService implements DirectoryServiceInterface
     public function listing(?string $path = '/'): array
     {
         $path = $this->getFullPath($path);
-        $contents = $this->fileSystem->cloud()->listContents($path)
-            ->sortByPath()
-            ->toArray();
+        $contents = $this->fileSystem->cloud()->listContents($path)->toArray();
+        $contents = $this->sortForListing($contents);
         
         return $this->formatAttributes($contents);
     }
